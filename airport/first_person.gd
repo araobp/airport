095c909c91796eeefb1cc52b7f8c8d792106133d
araobp <araobp@users.noreply.github.com>
2025-08-11 18:38:33 +0900

    feat: Add terminal and level assets
    This commit introduces the initial assets for the airport terminal and surrounding levels.
    
    Key changes:
    - Added a `Terminal` scene with a static body for collision.
    - Added a `Levels` scene.
    - Integrated the new scenes into the main `System.tscn`.
    - Enabled jumping in the first-person controller.
    - Switched to the Jolt Physics engine for better performance.
    - Included the source `.blend` files and exported `.glb` models for the terminal and levels.

--- airport/first_person.gd
+++ airport/first_person.gd
@@ -12,8 +12,6 @@ func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	
 func _input(event):
-	if event.is_action_pressed("ui_cancel"): get_tree().quit()
-	
 	if not Globals.chat_window_enabled and event is InputEventMouseMotion:
 			rotation.y -= event.relative.x * CAMERA_SENS
 			rotation.x -= event.relative.y * CAMERA_SENS
@@ -24,13 +22,13 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-	#	velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	if not Globals.chat_window_enabled:
+		# Handle jump.
+		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
 		var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 		var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 		if direction:
