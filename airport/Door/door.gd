4551c578a0f1569464a74691805821601a00b8f2
araobp <araobp@users.noreply.github.com>
2025-09-23 22:26:33 +0900

    feat: Update door control logic
    - Update door control script and scenes.
    - Include updated chat history.

--- airport/Door/door.gd
+++ airport/Door/door.gd
@@ -4,16 +4,11 @@ var forced_open = false
 @onready var state_machine = $AnimationTree["parameters/playback"]
 	
 func _process(_delta):
-	var visitors_in_the_area = false
-	for body in $SensingArea.get_overlapping_bodies():
-		if body.is_in_group("Visitors"):
-			visitors_in_the_area = true
-			if state_machine.get_current_node() != "Open":
-				state_machine.travel("Open")
-			
-	if !visitors_in_the_area:
-		if state_machine.get_current_node() == "Open" and not forced_open:
-			state_machine.travel("Close")
+	if len($SensingArea.get_overlapping_bodies()) > 0:
+		if state_machine.get_current_node() != "Open":
+			state_machine.travel("Open")
+	elif state_machine.get_current_node() == "Open" and not forced_open:
+		state_machine.travel("Close")
 		
 func door_control(control):
 	match control:
